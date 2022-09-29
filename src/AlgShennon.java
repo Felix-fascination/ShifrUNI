2f2700b169eae21b15ba846dc90065ba35b98fa0
Filex Uns <meseroigo@gmail.com>
2022-09-29 20:34:21 +0300

    Сделал нормальным объявление и заполнение словаря

--- src/AlgShennon.java
+++ src/AlgShennon.java
@@ -2,9 +2,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class AlgShennon {
     static final int LANGUAGE_POWER = 32;
@@ -13,6 +11,7 @@ public class AlgShennon {
         int hashLength = (int)(LANGUAGE_POWER/0.75);
         double shennonNumber;
         double overpower;
+        double keyEntropy;
 
         FileReader file = new FileReader("input.txt");
         Scanner sc = new Scanner(file);
@@ -20,55 +19,16 @@ public class AlgShennon {
         inputString = inputString.substring(0, inputString.indexOf(":"));
         char[] chars = inputString.toCharArray();
 
-
-        HashMap <Character, Integer> hm = languageInput(hashLength);
+        HashMap <Character, Integer> hm = new HashMap<>(hashLength);
         for (char ch: chars){
-            hm.put(ch, hm.get(ch) + 1);
-        }
+            if(!hm.containsKey(ch)) hm.put(ch, 1);
+            else hm.put(ch, hm.get(ch) + 1);
+         }
         shennonNumber = getShennonNumber( hm, inputString.length());
-        overpower = getOverpower(hm, shennonNumber);
+        overpower = getOverpower( shennonNumber);
+        keyEntropy = getShennonNumber(hm, inputString.length()) * inputString.length();
+        printToFile(inputString,  shennonNumber,  overpower, keyEntropy);
 
-        printToFile(inputString,  shennonNumber,  overpower);
-
-    }
-
-    private static HashMap<Character, Integer> languageInput(int mapSize){
-        int count = 0;
-        HashMap<Character, Integer> hm = new HashMap<>(mapSize);
-        hm.put('А', count);
-        hm.put('Б', count);
-        hm.put('В', count);
-        hm.put('Г', count);
-        hm.put('Д', count);
-        hm.put('Е', count);
-        hm.put('Ж', count);
-        hm.put('З', count);
-        hm.put('И', count);
-        hm.put('К', count);
-        hm.put('Л', count);
-        hm.put('М', count);
-        hm.put('Н', count);
-        hm.put('О', count);
-        hm.put('П', count);
-        hm.put('Р', count);
-        hm.put('С', count);
-        hm.put('Т', count);
-        hm.put('У', count);
-        hm.put('Ф', count);
-        hm.put('Х', count);
-        hm.put('Ц', count);
-        hm.put('Ч', count);
-        hm.put('Ш', count);
-        hm.put('Щ', count);
-        hm.put('Ъ', count);
-        hm.put('Ь', count);
-        hm.put('Ы', count);
-        hm.put('Э', count);
-        hm.put('Ю', count);
-        hm.put('Я', count);
-        hm.put(' ', count);
-
-        return hm;
     }
 
     private static double getShennonNumber(HashMap<Character, Integer> hm, int length){
@@ -85,15 +45,17 @@ public class AlgShennon {
         return resultNumber;
     }
 
-    private static void printToFile (String s, double shennonNumber, double overpower) throws FileNotFoundException {
+    private static void printToFile (String s, double shennonNumber, double overpower, double keyEntropy)
+            throws FileNotFoundException {
         PrintWriter pw = new PrintWriter(new FileOutputStream("output.txt"));
         pw.println("String is \""+ s + "\"" );
         pw.println("Its Shennon number is " + shennonNumber);
         pw.println("Its overpower is " + overpower + " бит/симв.");
+        pw.println("Key Entropy is " + keyEntropy);
         pw.close();
     }
 
-    private static double getOverpower(HashMap<Character, Integer> hm, double shennon){
+    private static double getOverpower( double shennon){
         double languageWorstPower = - Math.log((double)1/LANGUAGE_POWER)/Math.log(2);
         return languageWorstPower - shennon;
     }
